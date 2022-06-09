abb67f0a434c3d1b44dc94424317f58d658a7a98
mlbiam <marc@tremolo.io>
2022-06-09 12:46:17 -0400

    testing

--- build.sh
+++ build.sh
@@ -5,43 +5,43 @@ export VERSION="0.0.1"
 rm -rf target
 mkdir -p target
 
-env GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o ./target/ouctl-$VERSION-macos . 
-env GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o ./target/ouctl-$VERSION-linux .
-env GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o ./target/ouctl-$VERSION-win.exe .
+#env GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o ./target/ouctl-$VERSION-macos . 
+#env GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o ./target/ouctl-$VERSION-linux .
+#env GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o ./target/ouctl-$VERSION-win.exe .
 
-upx ./target/ouctl-$VERSION-macos
-upx ./target/ouctl-$VERSION-linux
-upx ./target/ouctl-$VERSION-win.exe
+#upx ./target/ouctl-$VERSION-macos
+#upx ./target/ouctl-$VERSION-linux
+#upx ./target/ouctl-$VERSION-win.exe
 
 
-mkdir target/darwin
-cp ./target/ouctl-$VERSION-macos target/darwin/ouctl
-chmod +x target/darwin/ouctl
-cp LICENSE target/darwin/
-cd target/darwin/
-zip ouctl-$VERSION-macos.zip ./ouctl LICENSE
-cd ../../
-mv target/darwin/ouctl-$VERSION-macos.zip target/
-rm -rf target/darwin
+#mkdir target/darwin
+#cp ./target/ouctl-$VERSION-macos target/darwin/ouctl
+#chmod +x target/darwin/ouctl
+#cp LICENSE target/darwin/
+#cd target/darwin/
+#zip ouctl-$VERSION-macos.zip ./ouctl LICENSE
+#cd ../../
+#mv target/darwin/ouctl-$VERSION-macos.zip target/
+#rm -rf target/darwin
 
-mkdir target/linux
-cp ./target/ouctl-$VERSION-linux target/linux/ouctl
-chmod +x target/linux/ouctl
-cp LICENSE target/linux/
-cd target/linux/
-zip ouctl-$VERSION-linux.zip ./ouctl LICENSE
-cd ../../
-mv target/linux/ouctl-$VERSION-linux.zip target/
-rm -rf target/linux
+#mkdir target/linux
+#cp ./target/ouctl-$VERSION-linux target/linux/ouctl
+#chmod +x target/linux/ouctl
+#cp LICENSE target/linux/
+#cd target/linux/
+#zip ouctl-$VERSION-linux.zip ./ouctl LICENSE
+#cd ../../
+#mv target/linux/ouctl-$VERSION-linux.zip target/
+#rm -rf target/linux
 
-mkdir target/win
-cp ./target/ouctl-$VERSION-win.exe target/win/ouctl.exe
-cp LICENSE target/win/
-cd target/win/
-zip ouctl-$VERSION-win.zip ./ouctl.exe ./LICENSE
-cd ../../
-mv target/win/ouctl-$VERSION-win.zip target/
-rm -rf target/win
+#mkdir target/win
+#cp ./target/ouctl-$VERSION-win.exe target/win/ouctl.exe
+#cp LICENSE target/win/
+#cd target/win/
+#zip ouctl-$VERSION-win.zip ./ouctl.exe ./LICENSE
+#cd ../../
+#mv target/win/ouctl-$VERSION-win.zip target/
+#rm -rf target/win
 
 
 
