6802d5d182be0d2845bc6d40fa58c30bf09d0ce8
mlbiam <marc@tremolo.io>
2022-06-09 12:39:14 -0400

    add deploy to s3

--- build.sh
+++ build.sh
@@ -53,7 +53,7 @@ export WIN_SHA256=$(shasum -a 256 ./target/ouctl-$VERSION-win.zip | awk '{print 
 
 cat ouctl.yaml | sed "s/_VERSION_/$VERSION/g" | sed "s/_MAC_SHA_/$MACOS_SHA256/g" | sed "s/_LINUX_SHA_/$LINUX_SHA256/g" | sed "s/_WIN_SHA_/$WIN_SHA256/g" > target/ouctl.yaml
 
-#aws s3 sync ./target/ s3://tremolosecurity-maven/repository/$1/
+aws s3 sync ./target/ s3://tremolosecurity-maven/repository/$1/
 
 
 
